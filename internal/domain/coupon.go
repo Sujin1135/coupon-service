90808b6886733485302701a810e108951b5936e6
Sujin1135 <mchead1@naver.com>
2025-03-27 02:46:44 +0900

    feat: added issued coupons field into Coupon struct

--- internal/domain/coupon.go
+++ internal/domain/coupon.go
@@ -6,13 +6,14 @@ import (
 )
 
 type Coupon struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	IssueAmount int64     `json:"issue_amount"`
-	IssuedAt    time.Time `json:"issued_at"`
-	ExpiresAt   time.Time `json:"expires_at"`
-	CreatedAt   time.Time `json:"created_at"`
-	ModifiedAt  time.Time `json:"modified_at"`
+	ID            string          `json:"id"`
+	Name          string          `json:"name"`
+	IssueAmount   int64           `json:"issue_amount"`
+	IssuedAt      time.Time       `json:"issued_at"`
+	ExpiresAt     time.Time       `json:"expires_at"`
+	IssuedCoupons []*IssuedCoupon `json:"issued_coupons"`
+	CreatedAt     time.Time       `json:"created_at"`
+	ModifiedAt    time.Time       `json:"modified_at"`
 }
 
 func NewCoupon(
